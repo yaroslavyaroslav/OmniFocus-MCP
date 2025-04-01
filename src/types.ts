66c6a0d7465552caecdc3fc07dbc55c9afca3ab2
ryan <ryan.mail.email@gmail.com>
2025-04-01 13:25:07 -0400

    working "database dump" tool.

---
+++ src/types.ts
@@ -0,0 +1,41 @@
+
+export interface OmnifocusTask {
+    id: string;
+    name: string;
+    note: string;
+    flagged: boolean;
+    
+    // Status
+    completed: boolean;
+    completionDate: string | null;
+    dropDate: string | null;
+    taskStatus: string; // One of Task.Status values
+    active: boolean;
+    
+    // Dates
+    dueDate: string | null;
+    deferDate: string | null;
+    estimatedMinutes: number | null;
+    
+    // Organization
+    tags: string[]; // Tag IDs
+    parentId: string | null;
+    containingProjectId: string | null;
+    projectId: string | null;
+    
+    // Task relationships
+    childIds: string[];
+    hasChildren: boolean;
+    sequential: boolean;
+    completedByChildren: boolean;
+    
+    // Attachments
+    attachments: any[]; // FileWrapper representations
+    linkedFileURLs: string[];
+    
+    // Notifications
+    notifications: any[]; // Task.Notification representations
+    
+    // Settings
+    shouldUseFloatingTimeZone: boolean;
+  }
